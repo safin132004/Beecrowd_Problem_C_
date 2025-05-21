bb649b14fe3575dc5945f72cc4ba1521f0d729c7
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-05-21 09:44:03 +0600

    Add files via upload

---
+++ 1009.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    char NAME;
+    double A , B , X , Y ;
+    scanf("%s", &NAME );
+
+
+    scanf("%lf", &A );
+    scanf("%lf", &B );
+    X = B * 0.15 ;
+    Y = X + A ;
+
+    printf("TOTAL = R$ %.2lf\n", Y );
+    return 0 ;
+
+}
