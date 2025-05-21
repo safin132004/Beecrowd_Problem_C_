fe9fbc58244b74e3ec7ebe2e970515eb463fb599
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-05-21 10:34:09 +0600

    Add files via upload

---
+++ 1011.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main ()
+{
+    double n = 3.14159 ;
+    int R ;
+    scanf("%d", &R);
+    double VOLUME =  (4.0/3) * n * R * R * R;
+    printf("VOLUME = %.3lf\n",VOLUME);
+    return 0 ;
+
+}
