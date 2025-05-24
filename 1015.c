66abadda84ffb3b0613d28da4ea6101d53467e69
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-05-24 17:37:19 +0600

    1015.c

--- 1015.c
+++ 1015.c
@@ -8,7 +8,7 @@ int main  ()
     scanf("%lf%lf",&x1,&y1);
     scanf("%lf%lf",&x2,&y2);
     double distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-    printf("%.4lf", distance);
+    printf("%.4lf\n", distance);
     return 0;
 }
 
