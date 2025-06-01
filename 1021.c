e1520634083d5541b38db7a2c2a8379ebf4a7aaa
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-06-01 13:21:01 +0600

    Add files via upload

---
+++ 1021.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+int main ()
+{
+    double X;
+    int Y;
+
+    scanf("%lf",&X);
+    Y = (int) (X * 100 );
+
+
+    int p100 =Y / 10000 ;
+     Y %= 10000 ;
+    int p50 = Y / 5000 ;
+     Y = Y % 5000 ;
+    int p20 = Y / 2000 ;
+     Y = Y % 2000 ;
+    int p10 = Y / 1000 ;
+     Y = Y % 1000 ;
+    int p5 = Y / 500 ;
+     Y = Y % 500 ;
+    int p2 = Y / 200 ;
+     Y = Y % 200 ;
+
+
+
+    int m100 = Y / 100 ;
+     Y = Y % 100 ;
+    int m50 = Y / 50 ;
+     Y = Y % 50 ;
+    int m20 = Y / 25 ;
+     Y = Y % 25 ;
+    int m10 = Y / 10 ;
+     Y = Y % 10 ;
+    int m5 = Y / 05 ;
+     Y = Y % 05 ;
+    int m1 = Y  ;
+
+
+    printf("NOTAS:\n");
+
+
+    printf("%d nota(s) de R$ 100.00\n", p100);
+    printf("%d nota(s) de R$ 50.00\n", p50);
+    printf("%d nota(s) de R$ 20.00\n", p20);
+    printf("%d nota(s) de R$ 10.00\n", p10);
+    printf("%d nota(s) de R$ 5.00\n", p5);
+    printf("%d nota(s) de R$ 2.00\n", p2);
+
+
+    printf("MOEDAS:\n");
+
+
+    printf("%d moeda(s) de R$ 1.00\n", m100);
+    printf("%d moeda(s) de R$ 0.50\n", m50);
+    printf("%d moeda(s) de R$ 0.25\n", m20);
+    printf("%d moeda(s) de R$ 0.10\n", m10);
+    printf("%d moeda(s) de R$ 0.05\n", m5);
+    printf("%d moeda(s) de R$ 0.01\n", m1);
+
+    return 0;
+
+}
+
