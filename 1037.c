50dcff258168be0a3109701bb343f97165012fbb
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-06-02 22:59:52 +0600

    Add files via upload

---
+++ 1037.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    double A ;
+    scanf("%lf",&A);
+
+    if (0<=A && 25>=A)
+    {
+        printf("Intervalo [0,25]\n");
+    }
+    else if (25<A && 50>=A)
+    {
+        printf("Intervalo (25,50]\n");
+    }
+    else if ((50<A && 75>=A))
+    {
+        printf("Intervalo (50,75]\n");
+    }
+    else if (75<A && 100>=A)
+    {
+        printf("Intervalo (75,100]\n");
+    }
+    else
+    {
+        printf("Fora de intervalo\n");
+    }
+
+
+
+     return 0;
+
+
+}
