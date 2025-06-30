0a5e0cad22a7728dab7e7cc06df962cf06449089
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-06-30 21:46:00 +0600

    Add files via upload

--- 1046.c
+++ 1046.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int main ()
 {
-    int A , B ;
-    scanf("%d%d", &A ,&B);
+    int A , B , C , D ;
+    scanf("%d%d%d%d", &A ,&C , &B , &D);
 
-    if (A==B)
+    if (A==B && C==D)
     {
-        printf("O JOGO DUROU 24 HORA(S)\n");
+        printf("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)\n");
     }
     else if (A < B)
     {
-        int d = B-A ;
-        printf("O JOGO DUROU %d HORA(S)\n", d);
+        int d = B-A , g = D - A;
+        printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", d , g );
     }
     else
     {
-        int s = ( 24 - A ) + B ;
+        int s = ( 24 - A ) + B , k = (60 - C) + D ;
         printf("O JOGO DUROU %d HORA(S)\n", s);
     }
     return 0;
