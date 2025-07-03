4cf34b427560265945ae0b5e03520a5d1f68d365
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-07-04 00:03:52 +0600

    Add files via upload

--- 1061.c
+++ 1061.c
@@ -1,40 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
-    int d1, h1, m1, s1;
-    int d2, h2, m2, s2;
+int main() {
+    float num, sum = 0.0;
+    int count = 0;
 
+    for (int i = 0; i < 6; i++) {
+        scanf("%f", &num);
 
-    scanf("Dia %d", &d1);
-    scanf("%d : %d : %d", &h1, &m1, &s1);
+        if (num > 0) {
+            sum += num;
+            count++;
+        }
+    }
 
-    scanf(" Dia %d", &d2);
-    scanf("%d : %d : %d", &h2, &m2, &s2);
-
-
-    int start_seconds = d1 * 86400 + h1 * 3600 + m1 * 60 + s1;
-    int end_seconds   = d2 * 86400 + h2 * 3600 + m2 * 60 + s2;
-
-
-    int total = end_seconds - start_seconds;
-
-
-    int days = total / 86400;
-    total %= 86400;
-
-    int hours = total / 3600;
-    total %= 3600;
-
-    int minutes = total / 60;
-    int seconds = total % 60;
-
-
-
-    printf("%d dia(s)\n", days);
-    printf("%d hora(s)\n", hours);
-    printf("%d minuto(s)\n", minutes);
-    printf("%d segundo(s)\n", seconds);
+    printf("%d valores positivos\n", count);
+    printf("%.1f\n", sum / count);
 
     return 0;
 }
