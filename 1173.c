9a706cd868f127b8800a4349046cf15fa3ad253d
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-07-15 23:48:14 +0600

    Add files via upload

---
+++ 1173.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+    int X , N[10];
+
+    scanf("%d",&X);
+
+    N[0] = X ;
+    printf("N[0] = %d", X);
+
+    for ( int i = 1 ; i < 10 ; i++ )
+    {
+        N[i] = N[i-1]*2 ;
+        printf("N[%d] = %d\n", i , N[i]);
+
+    }
+
+    return 0;
+
+
+
+}
