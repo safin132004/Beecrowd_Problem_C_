6d0a7c3789dbacfa3779675eb75ad47cf8a7747b
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-07-15 23:37:13 +0600

    Add files via upload

---
+++ 1065.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    int x , y=0 ;
+
+    for ( int i = 0 ; i < 5 ; i ++ )
+    {
+        scanf("%d", &x);
+        if (x%2 == 0)
+        {
+            y++;
+        }
+    }
+    printf("%d valores pares\n", y);
+    return 0;
+}
