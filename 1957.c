71d8ce5c1e980ce663bd5daa0cf8fa7b3e1e7e91
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-07-18 16:08:54 +0600

    Add files via upload

---
+++ 1957.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int V; // for the use of 64 bit
+    scanf("%u", &V); // Input the decimal number ....
+    printf("%X\n", V); // Print as hexadecimal in uppercase .... where %X declairs hexadecimal
+    return 0;
+}
+
