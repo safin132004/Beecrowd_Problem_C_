0ce5c4c9f1bf6904cec0472703afce8618fc8540
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-07-24 21:27:20 +0600

    Add files via upload

---
+++ 1185.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    float x[12][12], sum = 0.0;
+    int i, j;
+    char s;
+
+    scanf(" %c", &s);
+
+    for (i = 0; i < 12; i++)
+    {
+        for (j = 0; j < 12; j++)
+        {
+            scanf("%f", &x[i][j]);
+        }
+    }
+
+    for (i = 0; i < 12; i++)
+    {
+        for (j = 0; j < 12; j++)
+        {
+            if (i + j < 11)
+            {
+                sum += x[i][j];
+            }
+        }
+    }
+
+    if (s == 'M')
+    {
+        sum = sum / 66.0;
+
+        printf("%.1f\n", sum);
+
+        return 0;
+    }
+}
