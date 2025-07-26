f5d00c729befe63cb31a22350b45251e1f36b9a7
Saharia Hassan Safin <safinhassan164@gmail.com>
2025-07-26 19:57:09 +0600

    1064.c

--- 1064.c
+++ 1064.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    float x, sum = 0;
+    int count = 0;
 
+    for (int i = 0; i < 6; i++)
+    {
+        scanf("%f", &x);
+        if (x > 0)
+        {
+            sum += x;
+            count++;
+        }
+    }
+    float y = sum / count ; 
+
+    printf("%d valores positivos\n", count);
+    printf("%.1f\n", y );
+
+    return 0;
+}
